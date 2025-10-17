33d5c96e1174ac08efe05567f5808c8b7f404087
lmjuarezpacheco-lab <lmjuarezpacheco@gmail.com>
2025-10-16 20:12:03 -0600

    Delete IAM_Backend directory

--- IAM_Backend/IAM_Backend/app/static/guard.js
+++
@@ -1,8 +0,0 @@
-(function(){
-  const ok = localStorage.getItem('auth_ok') === 'true';
-  const exp = parseInt(localStorage.getItem('auth_expires')||'0',10);
-  if(!ok || Date.now() >= exp){
-    localStorage.clear();
-    location.replace('login.html');
-  }
-})();
